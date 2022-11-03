a466e0d29eccd44727f30e4fefaa08306d8a6edd
bleeq <bleeqtheproducer@gmail.com>
2022-11-03 19:57:15 +0900

    created directories and set a logger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,37 @@
 import express, {Application, Request, Response, NextFunction} from 'express'
+import mysql from 'mysql'
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '4585',
+})
+
+db.connect((err) => {
+    if (err) {
+        throw err
+    }
+    console.log('MySql Connected...')
+})
 
 const app: Application = express()
+
+// Create DB
+app.get('/createdb', (req, res) => {
+    let sql = 'create database instagramdb'
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw err
+        }
+        console.log(result)
+        res.send('database created...')
+    })
+
+})
 
 const add = (a: number, b: number): number => a + b 
 
 app.get('/', (req: Request, res: Response) => {
-    console.log(add(5, 5))
     res.send('Hello')
 })
 
